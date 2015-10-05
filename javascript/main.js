3ebf5a10b6950e5800de71cbc61d049f357a50b5
unknown <MRosich@MROSICH-EBF.mahercomm.com>
2015-10-04 21:05:03 -0400

    adding in more recipes, adding in image slider

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,11 @@
 /* CURRENTLY IN: javascript/main.js */
+
+(function(){
+
+	console.log('main.js connected');
+	$('.image-slider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+	});
+
+})(); // IIFE
